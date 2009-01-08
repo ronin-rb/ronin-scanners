98835896a4da2f50339f990bbceb483d4908e238
postmodern <postmodern.mod3@gmail.com>
2009-01-08 06:34:27 -0800

    Use the :flag option.

--- lib/ronin/scanners/nmap_task.rb
+++ lib/ronin/scanners/nmap_task.rb
@@ -28,123 +28,125 @@ module Ronin
     class NmapTask < RProgram::Task
 
       # TARGET SPECIFICATIONS:
-      short_option '-iL', :name => :target_file
-      short_option '-iR', :name => :random_targets
-      long_option '--exclude', :name => :exclude, :separator => ','
-      long_option '--excludefile', :name => :exclude_file
+      short_option :flag => '-iL', :name => :target_file
+      short_option :flag => '-iR', :name => :random_targets
+      long_option :flag => '--exclude', :name => :exclude, :separator => ','
+      long_option :flag => '--excludefile', :name => :exclude_file
 
       # HOST DISCOVERY:
-      short_option '-sL', :name => :list
-      short_option '-sP', :name => :ping
-      short_option '-PN', :name => :skip_discovery
-      short_option '-PS', :name => :syn_discovery
-      short_option '-PA', :name => :ack_discovery
-      short_option '-PU', :name => :udp_discovery
-      short_option '-PE', :name => :icmp_echo_discovery
-      short_option '-PP', :name => :icmp_timestamp_discovery
-      short_option '-PM', :name => :icmp_netmask_discovery
-      short_option '-PO', :name => :ip_ping
-      short_option '-n', :name => :disable_dns
-      short_option '-R', :name => :enable_dns
-      long_option '--dns-servers', :separator => ','
-      long_option '--system-dns'
+      short_option :flag => '-sL', :name => :list
+      short_option :flag => '-sP', :name => :ping
+      short_option :flag => '-PN', :name => :skip_discovery
+      short_option :flag => '-PS', :name => :syn_discovery
+      short_option :flag => '-PA', :name => :ack_discovery
+      short_option :flag => '-PU', :name => :udp_discovery
+      short_option :flag => '-PE', :name => :icmp_echo_discovery
+      short_option :flag => '-PP', :name => :icmp_timestamp_discovery
+      short_option :flag => '-PM', :name => :icmp_netmask_discovery
+      short_option :flag => '-PO', :name => :ip_ping
+      short_option :flag => '-n', :name => :disable_dns
+      short_option :flag => '-R', :name => :enable_dns
+      long_option :flag => '--dns-servers', :separator => ','
+      long_option :flag => '--system-dns'
 
       # SCAN TECHNIQUES:
-      short_option '-sS', :name => :syn_scan
-      short_option '-sT', :name => :connect_scan
-      short_option '-sA', :name => :ack_scan
-      short_option '-sW', :name => :window_scan
-      short_option '-sM', :name => :maimon_scan
-      short_option '-sU', :name => :udp_scan
-      short_option '-sN', :name => :null_scan
-      short_option '-sF', :name => :fin_scan
-      short_option '-sX', :name => :xmas_scan
-      long_option '--scanflags', :name => :tcp_scan_flags
-      short_option '-sI', :name => :idle_scan
-      short_option '-s0', :name => :ip_scan
-      short_option '-b', :name => :ftp_bounce_scan
-      long_option '--traceroute', :name => :traceroute
-      long_option '--reason', :name => :show_reason
+      short_option :flag => '-sS', :name => :syn_scan
+      short_option :flag => '-sT', :name => :connect_scan
+      short_option :flag => '-sA', :name => :ack_scan
+      short_option :flag => '-sW', :name => :window_scan
+      short_option :flag => '-sM', :name => :maimon_scan
+      short_option :flag => '-sU', :name => :udp_scan
+      short_option :flag => '-sN', :name => :null_scan
+      short_option :flag => '-sF', :name => :fin_scan
+      short_option :flag => '-sX', :name => :xmas_scan
+      long_option :flag => '--scanflags', :name => :tcp_scan_flags
+      short_option :flag => '-sI', :name => :idle_scan
+      short_option :flag => '-s0', :name => :ip_scan
+      short_option :flag => '-b', :name => :ftp_bounce_scan
+      long_option :flag => '--traceroute', :name => :traceroute
+      long_option :flag => '--reason', :name => :show_reason
 
       # PORT SPECIFICATION AND SCAN ORDER:
-      short_option '-p', :name => :ports
-      short_option '-F', :name => :fast
-      short_option '-r', :name => :consecutively
-      long_option '--top-ports'
-      long_option '--port-ratio'
+      short_option :flag => '-p', :name => :ports
+      short_option :flag => '-F', :name => :fast
+      short_option :flag => '-r', :name => :consecutively
+      long_option :flag => '--top-ports'
+      long_option :flag => '--port-ratio'
 
       # SERVICE/VERSION DETECTION:
-      short_option '-sV', :name => :service_scan
-      long_option '--version-intensity'
-      long_option '--version-light'
-      long_option '--version-all'
-      long_option '--version-trace'
+      short_option :flag => '-sV', :name => :service_scan
+      long_option :flag => '--version-intensity'
+      long_option :flag => '--version-light'
+      long_option :flag => '--version-all'
+      long_option :flag => '--version-trace'
 
       # SCRIPT SCAN:
-      short_option '-sC', :name => :default_script
-      long_option '--script'
-      long_option '--script-args', :name => :script_params, :separator => ','
-      long_option '--script-trace'
-      long_option '--script-updatedb', :name => :update_scriptdb
+      short_option :flag => '-sC', :name => :default_script
+      long_option :flag => '--script'
+      long_option :flag => '--script-args',
+                  :name => :script_params,
+                  :separator => ','
+      long_option :flag => '--script-trace'
+      long_option :flag => '--script-updatedb', :name => :update_scriptdb
 
       # OS DETECTION:
-      short_option '-O', :name => :os_fingerprint
-      long_option '--osscan_limit', :name => :limit_os_scan
-      long_option '--osscan_guess', :name => :max_os_scan
+      short_option :flag => '-O', :name => :os_fingerprint
+      long_option :flag => '--osscan_limit', :name => :limit_os_scan
+      long_option :flag => '--osscan_guess', :name => :max_os_scan
 
       # TIMING AND PERFORMANCE:
-      long_option '--min-hostgroup', :name => :min_host_group
-      long_option '--max-hostgroup', :name => :max_host_group
-      long_option '--min-parallelism'
-      long_option '--max-parallelism'
-      long_option '--min-rtt-timeout'
-      long_option '--max-rtt-timeout'
-      long_option '--max-retries'
-      long_option '--host-timeout'
-      long_option '--scan-delay'
-      long_option '--max-scan-delay'
-      long_option '--min-rate'
-      long_option '--max-rate'
+      long_option :flag => '--min-hostgroup', :name => :min_host_group
+      long_option :flag => '--max-hostgroup', :name => :max_host_group
+      long_option :flag => '--min-parallelism'
+      long_option :flag => '--max-parallelism'
+      long_option :flag => '--min-rtt-timeout'
+      long_option :flag => '--max-rtt-timeout'
+      long_option :flag => '--max-retries'
+      long_option :flag => '--host-timeout'
+      long_option :flag => '--scan-delay'
+      long_option :flag => '--max-scan-delay'
+      long_option :flag => '--min-rate'
+      long_option :flag => '--max-rate'
 
       # FIREWALL/IDS EVASION AND SPOOFING:
-      short_option '-f', :name => :packet_fragments
-      long_option '--mtu'
-      short_option '-D', :name => :decoys
-      short_option '-S', :name => :spoof
-      short_option '-e', :name => :interface
-      short_option '-g', :name => :source_port
-      long_option '--data-length'
-      long_option '--ip-options'
-      long_option '--ttl'
-      long_option '--spoof-mac'
-      long_option '--badsum', :name => :bad_checksum
+      short_option :flag => '-f', :name => :packet_fragments
+      long_option :flag => '--mtu'
+      short_option :flag => '-D', :name => :decoys
+      short_option :flag => '-S', :name => :spoof
+      short_option :flag => '-e', :name => :interface
+      short_option :flag => '-g', :name => :source_port
+      long_option :flag => '--data-length'
+      long_option :flag => '--ip-options'
+      long_option :flag => '--ttl'
+      long_option :flag => '--spoof-mac'
+      long_option :flag => '--badsum', :name => :bad_checksum
 
       # OUTPUT:
-      short_option '-oN', :name => :save
-      short_option '-oX', :name => :xml
-      short_option '-oS', :name => :skiddie
-      short_option '-oG', :name => :grepable
-      short_option '-v', :name => :verbose
-      long_option '--open', :name => :show_open_ports
-      long_option '--packet-trace', :name => :show_packets
-      long_option '--iflist', :name => :show_interfaces
-      long_option '--log-errors', :name => :show_log_errors
-      long_option '--append-output', :name => :append
-      long_option '--resume'
-      long_option '--stylesheet'
-      long_option '--webxml', :name => :nmap_stylesheet
-      long_option '--no-stylesheet', :name => :disable_stylesheet
+      short_option :flag => '-oN', :name => :save
+      short_option :flag => '-oX', :name => :xml
+      short_option :flag => '-oS', :name => :skiddie
+      short_option :flag => '-oG', :name => :grepable
+      short_option :flag => '-v', :name => :verbose
+      long_option :flag => '--open', :name => :show_open_ports
+      long_option :flag => '--packet-trace', :name => :show_packets
+      long_option :flag => '--iflist', :name => :show_interfaces
+      long_option :flag => '--log-errors', :name => :show_log_errors
+      long_option :flag => '--append-output', :name => :append
+      long_option :flag => '--resume'
+      long_option :flag => '--stylesheet'
+      long_option :flag => '--webxml', :name => :nmap_stylesheet
+      long_option :flag => '--no-stylesheet', :name => :disable_stylesheet
 
       # MISC:
-      short_option '-6', :name => :ipv6
-      short_option '-A', :name => :all
-      long_option '--datadir', :name => :nmap_datadir
-      long_option '--send-eth', :name => :raw_ethernet
-      long_option '--send-ip', :name => :raw_ip
-      long_option '--privledged'
-      long_option '--unprivleged'
-      short_option '-V', :name => :version
-      short_option '-h', :name => :help
+      short_option :flag => '-6', :name => :ipv6
+      short_option :flag => '-A', :name => :all
+      long_option :flag => '--datadir', :name => :nmap_datadir
+      long_option :flag => '--send-eth', :name => :raw_ethernet
+      long_option :flag => '--send-ip', :name => :raw_ip
+      long_option :flag => '--privledged'
+      long_option :flag => '--unprivleged'
+      short_option :flag => '-V', :name => :version
+      short_option :flag => '-h', :name => :help
 
     end
   end
