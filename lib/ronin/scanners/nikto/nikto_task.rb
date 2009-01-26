faae3f10164e6c82d3b919ee6d454981061ee325
postmodern <postmodern.mod3@gmail.com>
2009-01-26 13:27:54 -0800

    Forgot that short_option must have a :name passed to it.

--- lib/ronin/scanners/nikto/nikto_task.rb
+++ lib/ronin/scanners/nikto/nikto_task.rb
@@ -31,10 +31,10 @@ module Ronin
     class NiktoTask < RProgram::Task
 
       short_option :flag => '-h', :name => :host
-      short_option :flag => '-config'
+      short_option :flag => '-config', :name => :config
       short_option :flag => '-Cgidirs', :name => :cgi_dirs
       short_option :flag => '-cookies', :name => :print_cookies
-      short_option :flag => '-evasion'
+      short_option :flag => '-evasion', :name => :evasion
       short_option :flag => '-findonly', :name => :only_find
       short_option :flag => '-Format', :name => :format
 
@@ -54,19 +54,19 @@ module Ronin
       short_option :flag => '-id', :name => :http_auth
       short_option :flag => '-mutate', :name => :mutate_checks
       short_option :flag => '-nolookup', :name => :no_lookup
-      short_option :flag => '-output'
-      short_option :flag => '-port'
-      short_option :flag => '-root'
-      short_option :flag => '-ssl'
-      short_option :flag => '-timeout'
+      short_option :flag => '-output', :name => :output
+      short_option :flag => '-port', :name => :port
+      short_option :flag => '-root', :name => :root
+      short_option :flag => '-ssl', :name => :ssl
+      short_option :flag => '-timeout', :name => :timeout
       short_option :flag => '-useproxy', :name => :enable_proxy
-      short_option :flag => '-vhost'
+      short_option :flag => '-vhost', :name => :vhost
       short_option :flag => '-Version', :name => :version
 
       short_option :flag => '-dbcheck', :name => :validate_checks
-      short_option :flag => '-debug'
-      short_option :flag => '-update'
-      short_option :flag => '-verbose'
+      short_option :flag => '-debug', :name => :debug
+      short_option :flag => '-update', :name => :update
+      short_option :flag => '-verbose', :name => :verbose
 
     end
   end
