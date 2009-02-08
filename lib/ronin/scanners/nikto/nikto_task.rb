160b5b2dc08db9cfb23a45324a08e231c5915b91
postmodern <postmodern.mod3@gmail.com>
2009-02-08 14:57:15 -0800

    Added convenience methods for setting evasion options.

--- lib/ronin/scanners/nikto/nikto_task.rb
+++ lib/ronin/scanners/nikto/nikto_task.rb
@@ -59,6 +59,81 @@ module Ronin
       short_option :flag => '-Cgidirs', :name => :cgi_dirs
       short_option :flag => '-cookies', :name => :print_cookies
       short_option :flag => '-evasion', :name => :evasion
+
+      #
+      # Enable random URI encoding.
+      #
+      def random_uri_encoding!
+        self.evasion ||= ''
+        self.evasion << '1'
+      end
+
+      #
+      # Enable adding self-referencing directories (<tt>/./</tt>) to the
+      # request.
+      #
+      def directory_self_reference!
+        self.evasion ||= ''
+        self.evasion << '2'
+      end
+
+      #
+      # Enable premature URL ending.
+      #
+      def premature_url_ending!
+        self.evasion ||= ''
+        self.evasion << '3'
+      end
+
+      #
+      # Enable prepend long random strings to the request.
+      #
+      def prepend_random_strings!
+        self.evasion ||= ''
+        self.evasion << '4'
+      end
+
+      #
+      # Enable fake parameters to files.
+      #
+      def fake_params_to_files!
+        self.evasion ||= ''
+        self.evasion << '5'
+      end
+
+      #
+      # Enable using a tab character as the request spacer, instead of
+      # spaces.
+      #
+      def tab_request_spacer!
+        self.evasion ||= ''
+        self.evasion << '6'
+      end
+
+      #
+      # Enable random case sensitivity.
+      #
+      def random_casing!
+        self.evasion ||= ''
+        self.evasion << '7'
+      end
+
+      #
+      # Enable use of Windows style directory separators.
+      #
+      def windows_directories!
+        self.evasion ||= ''
+        self.evasion << '8'
+      end
+
+      #
+      # Enable session splicing.
+      #
+      def session_splicing!
+        self.evasion ||= ''
+        self.evasion << '9'
+      end
+
       short_option :flag => '-findonly', :name => :only_find
       short_option :flag => '-Format', :name => :format
 
