a817245207805f6cb73f6815f934b5dae1050dfe
Postmodern <postmodern.mod3@gmail.com>
2010-08-27 02:36:43 -0700

    Added Ronin::Scanners::WebVulnScanner.
    * Inherites from Ronin::Scanners::WebScanner.
    * Yields Ronin::Vulns::WebVuln resources which represent discovered
      web vulnerabilities.

---
+++ lib/ronin/scanners/web_vuln_scanner.rb
@@ -0,0 +1,119 @@
+#
+# Ronin Scanners - A Ruby library for Ronin that provides Ruby interfaces to
+# various third-party security scanners.
+#
+# Copyright (c) 2006-2010 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+require 'ronin/scanners/web_scanner'
+
+module Ronin
+  module Scanners
+    class WebVulnScanner < WebScanner
+
+      #
+      # The tests to perform on scanned pages.
+      #
+      # @return [Array<Proc>]
+      #   The tests to perform.
+      #
+      # @since 0.2.0
+      #
+      def WebVulnScanner.tests
+        @web_vuln_scanner_tests ||= []
+      end
+
+      #
+      # Adds a test to the {WebVulnScanner}. Tests should either return
+      # `nil` or a `Vulns::WebVuln` resource.
+      #
+      # @yield [scanner,page]
+      #   The given block will be passed the scanner and the page currently
+      #   being scanned.
+      #
+      # @yieldparam [WebVulnScanner] scanner
+      #   The scanner.
+      #
+      # @yieldparam [Spidr::Page] page
+      #   The page currently being scanned.
+      #
+      # @example
+      #   Ronin::Scanners::WebVulnScanner.test do |scanner,page|
+      #     # ...
+      #   end
+      #
+      # @since 0.2.0
+      #
+      def WebVulnScanner.test(&block)
+        WebVulnScanner.tests << block
+      end
+
+      protected
+
+      #
+      # Scans pages for web vulnerabilities.
+      #
+      # @yield [vuln]
+      #   The given block will be passed web vulnerabilities as they are
+      #   discovered.
+      #
+      # @yieldparam [Vulns::WebVuln, nil]
+      #   A newly discovered web vulnerability.
+      #
+      # @since 0.2.0
+      #
+      def scan
+        super do |page|
+          WebVulnScanner.tests.each do |block|
+            yield block.call(self,page)
+          end
+        end
+      end
+
+      #
+      # Normalizes the web vulnerability.
+      #
+      # @param [Vulns::WebVuln] vuln
+      #   The web vulnerability.
+      #
+      # @return [Vulns::WebVuln]
+      #   The web vulnerability.
+      #
+      # @since 0.2.0
+      #
+      def normalize_result(vuln)
+        vuln
+      end
+
+      #
+      # Prepares the web vulnerability to be saved into the Database.
+      #
+      # @param [Vulns::WebVuln] vuln
+      #   The web vulnerability.
+      #
+      # @return [Vulns::WebVuln]
+      #   The web vulnerability.
+      #
+      # @since 0.2.0
+      #
+      def new_resource(vuln)
+        vuln
+      end
+
+    end
+  end
+end
