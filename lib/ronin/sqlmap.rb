9f50449530ae6d466511ba53a672ab4087362281
Postmodern <postmodern.mod3@gmail.com>
2011-04-08 14:55:58 -0700

    Added SQLMap#scan and SQLMap.scan.

--- lib/ronin/sqlmap.rb
+++ lib/ronin/sqlmap.rb
@@ -19,11 +19,15 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
+require 'ronin/sqlmap_task'
 require 'ronin/config'
 
 require 'rprogram/program'
 
 module Ronin
+  #
+  # Represents a Ruby-ful interface to the `sqlmap` utility.
+  #
   class SQLMap < RProgram::Program
 
     # The directories to search within for `sqlmap.py`.
@@ -42,6 +46,8 @@ module Ronin
     # @raise [RProgram::ProgramNotFound]
     #   The `sqlmap.py` utility could not be found.
     #
+    # @since 1.0.0
+    #
     def self.find
       DIRS.each do |dir|
         path = File.join(Config::HOME,dir)
@@ -52,6 +58,52 @@ module Ronin
       end
 
       super()
+    end
+
+    #
+    # Finds and invokes the `sqlmap` utility.
+    #
+    # @param [Hash{Symbol => Object}] options
+    #   Options for `sqlmap`.
+    #
+    # @yield [sqlmap]
+    #   The given block will be passed the sqlmap task to be configured.
+    #
+    # @yieldparam [SQLMapTask] sqlmap
+    #   The sqlmap task to be configured.
+    #
+    # @return [Boolean]
+    #   Specifies whether the `sqlmap` utility exited successfully.
+    #
+    # @see #scan
+    #
+    # @since 1.0.0
+    #
+    def self.scan(options={},&block)
+      find.scan(options,&block)
+    end
+
+    #
+    # Invokes the `sqlmap` utility.
+    #
+    # @param [Hash{Symbol => Object}] options
+    #   Options for `sqlmap`.
+    #
+    # @yield [sqlmap]
+    #   The given block will be passed the sqlmap task to be configured.
+    #
+    # @yieldparam [SQLMapTask] sqlmap
+    #   The sqlmap task to be configured.
+    #
+    # @return [Boolean]
+    #   Specifies whether the `sqlmap` utility exited successfully.
+    #
+    # @see SQLMap
+    #
+    # @since 1.0.0
+    #
+    def scan(options={},&block)
+      run_task(SQLMapTask.new(options,&block))
     end
 
   end
