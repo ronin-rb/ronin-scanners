c21993ae4f254e22a487ecc8e6f0d9d6d4c0065a
Postmodern <postmodern.mod3@gmail.com>
2011-04-11 17:28:35 -0700

    Updated the copyright dates.

--- lib/ronin/ui/cli/commands/proxies.rb
+++ lib/ronin/ui/cli/commands/proxies.rb
@@ -2,7 +2,7 @@
 # Ronin Scanners - A Ruby library for Ronin that provides Ruby interfaces to
 # various third-party security scanners.
 #
-# Copyright (c) 2008-2010 Hal Brodigan (postmodern.mod3 at gmail.com)
+# Copyright (c) 2008-2011 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
