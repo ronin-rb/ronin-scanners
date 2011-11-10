305f538c099284fe3892efac7940a63ab19a15e5
Postmodern <postmodern.mod3@gmail.com>
2011-11-10 02:22:50 -0500

    Added wordlist helper methods to CredentialScanner.

--- lib/ronin/scanners/credential_scanner.rb
+++ lib/ronin/scanners/credential_scanner.rb
@@ -19,6 +19,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
+require 'ronin/extensions/file'
 require 'ronin/scanners/scanner'
 require 'ronin/credential'
 require 'ronin/fuzzing'
@@ -29,7 +30,100 @@ module Ronin
     # The {CredentialScanner} class represents scanners that yield
     # `Credential` results and resources.
     #
+    # @since 1.0.0
+    #
     class CredentialScanner < Scanner
+
+      # Paths to a word-list file(s)
+      parameter :wordlists, :type => Array[String],
+                            :default => [],
+                            :description => 'Wordlist file(s) to use'
+
+      # Mutation rules to apply to words from the {#wordlist}
+      parameter :mutations, :default => {},
+                            :description => 'Hash of mutations to perform'
+
+      # String generator template
+      parameter :generator, :type => Array,
+                            :description => 'String generator template'
+
+      protected
+
+      #
+      # Reads each line from a wordlist file.
+      #
+      # @param [String] path
+      #   The path to the wordlist file.
+      #
+      # @yield [word]
+      #   The given block will be passed each word from the file.
+      #
+      # @yieldparam [String] word
+      #   A word from the wordlist.
+      #
+      # @return [Enumerator]
+      #   If no block is given, an Enumerator will be returned.
+      #
+      # @api semipublic
+      #
+      def wordlist(path,&block)
+        File.each_line(path,&block)
+      end
+
+      #
+      # Iterates over each word from the {#wordlist} or String {#generator}.
+      #
+      # @yield [word]
+      #   The given block will be passed each word.
+      #
+      # @yieldparam [String] word
+      #   A word from the {#wordlist} or String {#generator}.
+      #
+      # @return [Enumerator]
+      #   If no block is given, an Enumerator will be returned.
+      #
+      # @api semipublic
+      #
+      def each_word(&block)
+        return enum_for(:each_word) unless block
+
+        if self.wordlists
+          self.wordlists.each do |path|
+            wordlist(path) do |word|
+              unless self.mutations.empty?
+                word.mutate(mutations,&block)
+              else
+                yield word
+              end
+            end
+          end
+        end
+
+        if self.generator
+          String.generate(self.generator,&block)
+        end
+      end
+
+      #
+      # Iterates over every n words.
+      #
+      # @param [Integer, Range, Array] n
+      #   The number of words to combine.
+      #
+      # @yield [words]
+      #   The given block will be passed every combination of `n` words.
+      #
+      # @yieldparam [String]
+      #    The combination of `n` words.
+      #
+      # @return [Enumerator]
+      #   If no block is given, an Enumerator will be returned.
+      #
+      # @api semipublic
+      #
+      def each_n_words(n,&block)
+        String.generate([each_word, n],&block)
+      end
 
     end
   end
