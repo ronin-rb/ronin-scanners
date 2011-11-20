5571b4b1b308fbebbac730f8a163462fbf4646fc
Postmodern <postmodern.mod3@gmail.com>
2011-11-20 02:19:40 -0800

    Added a command for the Database Credential scanner.

---
+++ lib/ronin/ui/cli/commands/scanners/database.rb
@@ -0,0 +1,56 @@
+#
+# Ronin Scanners - A Ruby library for Ronin that provides Ruby interfaces to
+# various third-party security scanners.
+#
+# Copyright (c) 2008-2011 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+require 'ronin/ui/cli/credential_scanner_command'
+require 'ronin/scanners/database'
+
+module Ronin
+  module UI
+    module CLI
+      module Commands
+        module Scanners
+          class Database < CredentialScannerCommand
+
+            desc 'Performs bruteforcing against a Database server'
+
+            # The port that Database is listening on
+            scanner_option :port, :aliases => '-p'
+
+            # The host that is running Database
+            argument :host, :required => true
+
+            #
+            # Runs the {Ronin::Scanners::Database} scanner.
+            #
+            def execute
+              print_info 'Saving captured Database credentials ...' if options.saved?
+
+              scan!
+
+              print_info 'All valid Database credentials saved.' if options.save?
+            end
+
+          end
+        end
+      end
+    end
+  end
+end
